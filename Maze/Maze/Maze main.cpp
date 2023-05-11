981642bc058612225567a1600c12cfb19e8cafe8
renbohan <1551608379@qq.com>
2023-05-11 09:48:22 +0800

    完成自己的队列类编写

--- Maze/Maze/Maze main.cpp
+++ Maze/Maze/Maze main.cpp
@@ -1,6 +1,22 @@
-#include"iostream"
+#include <iostream>
+#include "MyQueue.h"
+
 using namespace std;
-int main()
-{
-	cout << "Hello this is MAZE" << endl;
+
+void ShowWelcome() {
+  cout << "===========================================" << endl;
+  cout << "|               Maze Game                 |" << endl;
+  cout << "|        Powered by BFS algorithm         |" << endl;
+  cout << "|-----------------------------------------|" << endl;
+  cout << "|        [1] Search & Show Mode           |" << endl;
+  cout << "|       [2] Step by Step Show Mode        |" << endl;
+  cout << "===========================================" << endl;
+}
+
+MyQueue<int> q;
+
+int main() {
+  ShowWelcome();
+  
+  return 0;
 }
---
+++ Maze/Maze/MyQueue.h
@@ -0,0 +1,63 @@
+#pragma once
+
+template <typename T>
+class Node {
+public:
+  T data;
+  Node* next;
+  Node() {
+    next = 0;
+    T = T();
+  }
+  Node(T _data) {
+    next = 0;
+    data = _data;
+  }
+};
+
+
+template <typename T>
+class MyQueue {
+  Node<T>* queueHead, * queueTail;
+public:
+  MyQueue() {
+    queueHead = queueTail = 0;
+  }
+  void Push(T elem) {
+    Node<T>* newNode = new Node<T>(elem);
+    if (queueTail) {
+      queueTail->next = newNode;
+      queueTail = newNode;
+    }
+    else {
+      queueHead = newNode;
+      queueTail = newNode;
+    }
+  }
+  T Front() {
+    if (queueHead) {
+      return queueHead->data;
+    }
+    else {
+      throw "Empty Queue, Do not call Top()!";
+    }
+  }
+  bool Empty() {
+    return queueHead == 0;
+  }
+  void Pop() {
+    if (queueHead == 0) {
+      throw "Empty Queue, Do not call Pop()!";
+    }
+    // 只有一个元素的时候
+    if (queueHead == queueTail) {
+      delete queueHead;
+      queueHead = queueTail = 0;
+      return;
+    }
+    // 有多个元素，释放head，后移head指针
+    Node<T>* tmpNext = queueHead->next;
+    delete queueHead;
+    queueHead = tmpNext;
+  }
+};
